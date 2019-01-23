010b3e5d50a605d91f7fac25453bd457a76aa1ba
sknoll1818 <43442575+sknoll1818@users.noreply.github.com>
2019-01-23 15:34:05 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,40 +1,102 @@
-function CellChoice(){
+function cellChoice() {
+  var buffer = document.getElementById('buf').value;
+  var piece2 = document.getElementById('piece2').value;
+  var piece3 = document.getElementById('piece3').value;
+  var oTable = document.getElementById('UFTable');
 
-    var piece = document.getElementsByName('PieceType');
+  var Row = document.getElementById(piece2);
+  var Cells = Row.getElementsByTagName('td');
+  var comm;
+  switch (piece3) {
+    case "UR":
+      comm = Cells[2].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "UB":
+      comm = Cells[1].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "UL":
+      comm = Cells[3].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "FR":
+      comm = Cells[4].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "FD":
+      comm = Cells[5].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "FL":
+      comm = Cells[6].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "DF":
+      comm = Cells[7].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "DR":
+      comm = Cells[8].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "DB":
+      comm = Cells[8].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "DL":
+      comm = Cells[10].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "RB":
+      comm = Cells[11].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "RD":
+      comm = Cells[12].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "RF":
+      comm = Cells[13].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "RU":
+      comm = Cells[14].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "LB":
+      comm = Cells[15].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "LU":
+      comm = Cells[16].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "LF":
+      comm = Cells[17].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "LD":
+      comm = Cells[18].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "BD":
+      comm = Cells[19].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "BR":
+      comm = Cells[20].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "BU":
+      comm = Cells[21].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "BL":
+      comm = Cells[22].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+  }
+  return null;
 
-    var x = document.getElementById("frm");
-    var text = "";
-    var i;
-    for (i = 7; i < x.length-1 ;i++) {
-        text += x.elements[i].value+" ";
-    }
-    alert(text);
-    var columns = [
-  [UBR,C],
-  [UBL,B],
-  //add more columns
-  //then add rows
-];
-}
-var excelIO = new GC.Spread.Excel.IO();
-var excelUrl = $("#importUrl").val();
-function ImportFile() {
-    var excelUrl = "./Full 3Style Comm Series + more.xlsx";
-
-    var oReq = new XMLHttpRequest();
-    oReq.open('get', excelUrl, true);
-    oReq.responseType = 'blob';
-    oReq.onload = function () {
-        var blob = oReq.response;
-        excelIO.open(blob, LoadSpread, function (message) {
-            console.log(message);
-        });
-    };
-    oReq.send(null);
-}
-function LoadSpread(json) {
-    jsonData = json;
-    workbook.fromJSON(json);
-
-    workbook.setActiveSheet("Revenues (Sales)");
 }
