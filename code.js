2b44712fd7b1fa90b2a2bc1d2f2f5ff0608a4653
sknoll1818 <43442575+sknoll1818@users.noreply.github.com>
2019-04-01 11:46:41 -0400

    Added DF Support

--- code.js
+++ code.js
@@ -1,12 +1,104 @@
 function cellChoice() {
   var buffer = document.getElementById('buf').value;
-  var piece2 = buffer+document.getElementById('piece2').value;
+  var piece2 = buffer + document.getElementById('piece2').value;
   var piece3 = document.getElementById('piece3').value;
 
   var Row = document.getElementById(piece2);
   var Cells = Row.getElementsByTagName('td');
   var comm;
   switch (buffer) {
+    case "DF":
+      switch (piece3) {
+        case "BU":
+          comm = Cells[1].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "RU":
+          comm = Cells[2].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "FU":
+          comm = Cells[3].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "LU":
+          comm = Cells[4].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "UF":
+          comm = Cells[5].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "RF":
+          comm = Cells[6].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "LF":
+          comm = Cells[7].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "RD":
+          comm = Cells[8].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "BD":
+          comm = Cells[9].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "LD":
+          comm = Cells[10].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "BR":
+          comm = Cells[11].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "DR":
+          comm = Cells[12].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "FR":
+          comm = Cells[13].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "UR":
+          comm = Cells[14].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "BL":
+          comm = Cells[15].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "UL":
+          comm = Cells[16].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "FL":
+          comm = Cells[17].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "DL":
+          comm = Cells[18].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "DB":
+          comm = Cells[19].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "RB":
+          comm = Cells[20].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "UB":
+          comm = Cells[21].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+        case "LB":
+          comm = Cells[22].innerHTML;
+          document.getElementById('Comm').innerHTML = comm;
+          break;
+      }
+      break;
     case "UF":
       switch (piece3) {
         case "UR":
@@ -99,7 +191,7 @@ function cellChoice() {
           break;
       }
       break;
-    case "UFR" :
+    case "UFR":
       switch (piece3) {
         case "UBL":
           comm = Cells[2].innerHTML;
@@ -188,5 +280,5 @@ function cellChoice() {
         default:
           alert("The setup you gave was invalid");
       }
-    }
+  }
 }
